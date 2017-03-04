d133a0ab30534d64e4b09965301b108e39bde150
Fredrik Hansson <fredrikhansson_12345@yahoo.com>
2017-03-04 14:41:59 +0100

    initial commit

---
+++ src/mainquiet.cpp
@@ -0,0 +1,11 @@
+#include "include1.h"
+#include "include2.h"
+
+int main(int argc, char* argv[])
+{
+    importantthing s1;
+    something2 s3;
+    s1.x = OPTION1;
+    s1.y = OPTION2;
+    return 0;
+}
